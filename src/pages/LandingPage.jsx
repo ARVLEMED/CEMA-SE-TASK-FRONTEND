7bfa254f39cb8fc36843153f163bd5f5f0c343fb
aron k koech <varokoaruna81@gmail.com>
2025-04-27 15:42:59 +0300

    adds the project power point file with button to open it

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,6 +12,11 @@ function LandingPage() {
       {/* Hero Section */}
       <section className="hero">
         <img src="../src/assets/hospital.jpg" alt="Hospital Building" className="hero-img" />
+        
+        {/* Button for opening PowerPoint in the browser */}
+        <a href="/files/project-presentation.pptx.pdf" target="_blank" rel="noopener noreferrer">
+          <button className="download-btn">Open the Project PowerPoint</button>
+        </a>
       </section>
 
       {/* Footer */}
